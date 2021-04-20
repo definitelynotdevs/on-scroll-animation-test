0ae15396998faeae1640384cfe3b0ed23a1df4db
Tomek Kielar <tomek.kielar00@gmail.com>
2021-04-20 22:55:57 +0200

    scroll animation with lottie reveal animation for content

--- src/views/home.js
+++ src/views/home.js
@@ -1,83 +1,51 @@
 import Layout from "layout/default";
-import React from "react";
+import React, { useEffect, useRef } from "react";
+import Lottie from "lottie-react";
+import lottie from "lottie-web";
+import animation from "animations/animation.json";
+import Fade from "react-reveal/Fade";
+
+import { Controller, Scene } from 'react-scrollmagic';
+
+const interactivity = {
+  mode: "scroll",
+  actions: [
+    {
+      visibility: [0, 0.1],
+      type: 'stop',
+      frames: [10],
+    },
+    {
+      visibility: [0.1, 0.9],
+      type: 'seek',
+      frames: [10, 199],
+    },
+    {
+      visibility: [0.9, 1],
+      type: 'stop',
+      frames: [200],
+    },
+    
+  ],
+};
 
 export const Home = () => {
+
   return (
     <Layout>
-      <div className="flex flex-col items-center justify-center min-h-screen py-2">
-        <main className="flex flex-col items-center justify-center flex-1 px-20 text-center">
-          <h1 className="text-6xl font-bold">
-            Welcome to{" "}
-            <a className="text-blue-600" href="https://nextjs.org">
-              Next.js!
-            </a>
-          </h1>
-
-          <p className="mt-3 text-2xl">
-            Get started by editing{" "}
-            <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-              pages/index.js
-            </code>
-          </p>
-
-          <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-            <a
-              href="https://nextjs.org/docs"
-              className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-            >
-              <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-              <p className="mt-4 text-xl">
-                Find in-depth information about Next.js features and API.
-              </p>
-            </a>
-
-            <a
-              href="https://nextjs.org/learn"
-              className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-            >
-              <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-              <p className="mt-4 text-xl">
-                Learn about Next.js in an interactive course with quizzes!
-              </p>
-            </a>
-
-            <a
-              href="https://github.com/vercel/next.js/tree/master/examples"
-              className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-            >
-              <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-              <p className="mt-4 text-xl">
-                Discover and deploy boilerplate example Next.js projects.
-              </p>
-            </a>
-
-            <a
-              href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-            >
-              <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-              <p className="mt-4 text-xl">
-                Instantly deploy your Next.js site to a public URL with Vercel.
-              </p>
-            </a>
-          </div>
-        </main>
-
-        <footer className="flex items-center justify-center w-full h-24 border-t">
-          <a
-            className="flex items-center justify-center"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Powered by{" "}
-            <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
-          </a>
-        </footer>
+      <div className="flex flex-col items-center justify-center min-h-screen">
+        <div className="h-[700px]">Scroll Down to see the magic ;)</div>
+        <Lottie animationData={animation} interactivity={interactivity} className="h-96"/>
+        <div className="h-[700px]"/>
+        <Fade left>
+        <div className="w-full">
+          <img src={'https://img.rebenagesta.com/tony-montana-biuro.jpg'}/>
+        </div>
+        </Fade>
+        
       </div>
     </Layout>
   );
 };
 
-
-export default Home
+export default Home;
